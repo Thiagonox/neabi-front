23fff47256948f83304fb4a43ab00df29559696f
Thiago Gentil <71569140+Thiagonox@users.noreply.github.com>
2022-12-20 03:26:06 +0000

    update Artigos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,15 +18,15 @@ function Header() {
             <div className='line-menu'></div>
         </div>
         <Link to='/'><img className='h-8' src={logo} alt="" /></Link>
-        <nav className={`absolute rounded-xl top-16 w-52 h-4/5 left-0 bg-white-custom flex justify-center items-center m-auto sm:static z-0`}>
+        <nav className={`absolute rounded-xl top-16 left-0 bg-white-custom flex justify-center items-center m-auto sm:static z-0`}>
           <ul className='h-full sm:flex flex-row sm:justify-evenly items-center'>
                 <li className='txt-menu hover:text-black'><NavLink className={(navInfo) => (navInfo.isActive ? 'border-b-2 border-black text-red-custom' : '') } to='/'>HOME</NavLink></li>
                 <li className='txt-menu hover:text-black'><NavLink className={(navInfo) => (navInfo.isActive ? 'border-b-2 border-black text-red-custom' : '')} to='/Artigos'>ARTIGOS</NavLink></li>
           </ul>
         </nav>
-        <div className='w-8 h-8 flex justify-center border-2 border-orange-custom rounded-full overflow-hidden cursor-pointer'>
+        <buttom className='w-8 h-8 flex justify-center border-2 border-orange-custom rounded-full overflow-hidden cursor-pointer'>
           <img className='h-7' src={user} alt="" />
-        </div>
+        </buttom>
       </div>
       <hr className=' h-0.5 bg-black'/>
     </header>
--- src/components/artigos/Artigo.jsx
+++ src/components/artigos/Artigo.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Artigo(props) {
   return (
-    <div className={`pb-8 hover:bg-green-custom cursor-pointer duration-500 text-white-custom ${props.color}`}>
+    <div className={`pb-8 hover:bg-green-custom duration-500 text-white-custom ${props.color}`}>
         <hr className='w-11/12 border-t-2 border-red-custom m-auto mb-4' />
         <div className='w-11/12 p-4 m-auto flex items-center justify-between'>
             <h2 className='md:w-1/4 w-[55%] font-Perandory text-2xl md:text-3xl mr-2'>{props.title}</h2>
             <p className='md:w-2/4 hidden md:block font-Bluu text-xl'>{props.text}</p>
-            <div className='w-20 h-20 rounded-3xl flex items-center bg-white-custom border-red border-t-2'>
+            <a href={props.link} download className='w-16 h-16 rounded-3xl flex items-center bg-white-custom border-red border-t-2'>
               <img className='w-14 m-auto hover:animate-bounce' src="../src/assets/icons/arrow.svg" alt="" />
-            </div>
+            </a>
         </div>
     </div>
   )
